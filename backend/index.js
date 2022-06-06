ef0c4f16e391064624d2a743e7e0082894784c57
zicoz18 <zicoz18@ku.edu.tr>
2022-06-06 17:37:34 +0300

    Restructer code to include both frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const main = () => {
+	const mysql_connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+	});
+	mysql_connection.connect();
+
+	const app = express();
+	app.use(bodyParser.json());
+	app.use(cors({ origin: true }));
+
+	const PORT = 8080;
+
+	app.get("/api", (req, res) => {
+		const queryString = req.query.queryString;
+		if (!queryString) {
+			res.status(400).send("queryString is required");
+		}
+		console.log("recieved query: ", queryString);
+
+		mysql_connection.query(queryString, (err, result, fields) => {
+			if (err) {
+				res.status(400).send({
+					type: "error",
+					message: err.message,
+				});
+			}
+			res.status(200).send({
+				type: "success",
+				data: result,
+			});
+		});
+	});
+
+	app.listen(PORT, () => console.log(`Listening at localhost: ${PORT}`));
+};
+
+main();
+
+// const mysql = require("mysql2");
+
+// const connectAndQuery = async () => {
+// 	const mysql_connection = mysql.createConnection({
+// 		host: "localhost",
+// 		user: "root",
+// 		password: "CoderZiya1305",
+// 		database: "mini_imdb",
+// 	});
+// 	mysql_connection.connect();
+
+// 	mysql_connection.query("SELECT * FROM CAST", (err, result, fields) => {
+// 		console.log("res: ", result);
+// 	});
+// };
+
+// export default connectAndQuery;
